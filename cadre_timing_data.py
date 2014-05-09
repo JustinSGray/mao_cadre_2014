1f06a8842806644127ae7ac43e5ae164e0a9007d
Justin Gray <justin.s.gray@gmail.com>
2014-05-09 10:37:11 -0400

    new charts

--- cadre_timing_data.py
+++ cadre_timing_data.py
@@ -1,50 +1,47 @@
-"""N = 750
-Problem Size (CADRE) = 179205
-Time = 126 sec
 
-N = 1500
-Problem Size (CADRE) = 358305
-Time = 254 sec
-
-N = 3000
-Problem Size (CADRE) = 716505
-Time = 539 sec
-
-
-With Pruning and Zero-vector Detection
-----------------------------------------
-
-N = 750
-Problem Size (CADRE) = 146942
-Time = 86.6 sec
-
-N = 1500
-Problem Size (CADRE) = 293792
-Time = 173 sec
-
-N = 3000
-Problem Size (CADRE) = 587492
-Time = 364 sec"""
 
 
 import numpy as np
 from matplotlib import pyplot as plt
-
-full_graph_vars = np.array([179205, 358305, 716505])*6
-full_graph_times = np.array([126,254,539])
+from matplotlib.ticker import FuncFormatter
 
 
-prune_graph_vars = np.array([146942, 293792, 587492])*6
-prune_graph_times = np.array([86.6,173,364])
+full_graph_vars = np.array([179205, 358305, 716505,1432905, 2865705],dtype=float)*6
+full_graph_times = np.array([96,191,404,863,1934])
 
 
-plt.plot(full_graph_vars, full_graph_times)
-plt.plot(prune_graph_vars, prune_graph_times)
-plt.xlim([10**5.5,10**6.6])
-plt.xticks([10**5.5,10**6,10**6.5])
+
+prune_graph_vars = np.array([146942, 293792, 587492,1174892,2349692])*6
+
+prune_graph_times = np.array([76,149,306,635,1375])
+
+
+plt.rcParams.update({
+    'font.size': 15,
+    'font': 'serif' 
+    })
+
+fig = plt.figure()
+
+plt.loglog(full_graph_vars, full_graph_times)
+plt.loglog(full_graph_vars, prune_graph_times)
+plt.xlim([10**6,2*10**7])
+plt.ylim([5*10**1,2.5*10**3])
+plt.xlabel('Number of Variables')
+plt.ylabel('Time (sec)')
+#plt.xticks([10**5.5,10**6,10**7.5])
 
 ax = plt.gca()
-ax.set_yscale('log')
-ax.set_xscale('log')
 
-plt.show()
+def sci(x, pos):
+    'The two args are the value and tick position'
+    return '%.0e' % (x)
+
+ax.xaxis.set_major_formatter(FuncFormatter(sci))
+
+#plt.show()
+
+fig.set_size_inches(6,5)
+fig.tight_layout()
+plt.savefig('cadre_var_scaling.pdf', dpi=1000,bbox_inches="tight")
+
